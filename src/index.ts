318207f689ae6b7c371d9d4e0dd4a8710d3ac1e9
william phillpotts <william.phillpotts@googlemail.com>
2023-04-27 19:58:53 +0100

    add: prettier

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@
  * The sum of passed numbers
  * @param a first number to be added
  * @param b second number to be added
- * @returns the sum of the numbers  
+ * @returns the sum of the numbers
  */
-export const sum = (a: number, b:number): number => {
+export const sum = (a: number, b: number): number => {
     return a + b
 }
